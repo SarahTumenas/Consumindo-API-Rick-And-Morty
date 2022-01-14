0b2e7242bc304888ac3fa080144ad062d8f3b36b
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-14 16:55:45 -0300

    Capturar elementos Html manipuláveis

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const img = document.querySelector('img#personagem')
+const nomeDoPersonagem = document.getElementById('nomeDoPersonagem')
+const especie = document.getElementById('especie')
+const status = document.getElementById('status')
+
 function refreshPage() {
   window.location.reload()
 }
