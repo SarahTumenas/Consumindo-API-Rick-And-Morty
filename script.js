a3e2677301a174cc835fada7ae703e6c1d50a55b
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-14 18:55:01 -0300

    Refatorando HTML,CSS e JS para adequação ao Desafio pedido pela Zup

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-const img = document.querySelector('img#personagem')
-const nomeDoPersonagem = document.getElementById('nomeDoPersonagem')
-const especie = document.getElementById('especie')
-const status = document.getElementById('status')
+const imagem = document.querySelector('img')
+const botao = document.querySelector('button')
+const nomeDoPersonagem = document.querySelector('#nome')
+const especie = document.querySelector('#especie')
+const condicao = document.querySelector('#status')
 
-function refreshPage() {
-  window.location.reload()
+traduzirCondicao = data => {
+  if (data.status == 'unknown') {
+    return 'Não sabemos'
+  } else if (data.status == 'Alive') {
+    return 'Sim'
+  } else {
+    return 'Não. Está morto'
+  }
 }
+
+gerarValorAletorio = () => {
+  return Math.floor(Math.random() * 671)
+}
+
+pegarPersonagem = () => {
+  let numeroAleatorio = gerarValorAletorio()
+  return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      imagem.src = data.image
+      imagem.alt = data.name
+      nomeDoPersonagem.innerHTML = data.name
+      especie.innerHTML = data.species
+      condicao.innerHTML = traduzirCondicao(data)
+    })
+}
+
+botao.onclick = pegarPersonagem
